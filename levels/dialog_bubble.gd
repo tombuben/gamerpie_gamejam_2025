d2183b8aca9e8b125bbdc71160fec8d1dc77ba29
LordWizardy <l.wizardy@gmail.com>
2025-01-24 23:44:00 +0100

    Added script to bubble slot
    Now npc speech bubble takes dialogue from specific npc

---
+++ levels/dialog_bubble.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+@export var lines: Array[String] = [
+	"This is a test dialogue."
+]
+
+@export var checkValue = "Test"
--- npc_character.gd
+++ npc_character.gd
@@ -2,15 +2,11 @@ extends Node2D
 
 @onready var bubble_slot : Node2D = $BubbleSlot
 
-const lines: Array[String] = [
-	"This is a test dialogue."
-]
-
 signal npc_triggered
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print(body.name)
 	emit_signal("npc_triggered")
-	DialogManager.start_dialog(global_position, lines)
+	DialogManager.start_dialog(global_position, $BubbleSlot.lines)
 	print("npc triggered")
 	print(name)
