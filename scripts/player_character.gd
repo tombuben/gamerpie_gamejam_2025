a397bdb35004ddb2371e8ca1d4b766f102fa73c9
Tomas Bubenicek <tom@tombuben.eu>
2025-01-24 22:37:23 +0100

    Made a static body for the movement

--- scripts/player_character.gd
+++ scripts/player_character.gd
@@ -1,15 +1,25 @@
 extends CharacterBody2D
-@export var player_speed = 100
 
-func _process(delta: float) -> void:
-	
-	var direction = Vector2(
-   	 Input.get_action_strength("right") - Input.get_action_strength("left"),
-   	 Input.get_action_strength("down") - Input.get_action_strength("up")
-	)
+@export var player_speed = 200
+@export var friction = 0.01
+@export var acceleration = 0.1
 
-	direction = direction.normalized()
+func get_input():
+	var input = Vector2()
+	if Input.is_action_pressed('right'):
+		input.x += 1
+	if Input.is_action_pressed('left'):
+		input.x -= 1
+	if Input.is_action_pressed('down'):
+		input.y += 1
+	if Input.is_action_pressed('up'):
+		input.y -= 1
+	return input
 
-	velocity = direction * player_speed
-
+func _physics_process(delta):
+	var direction = get_input()
+	if direction.length() > 0:
+		velocity = velocity.lerp(direction.normalized() * player_speed, acceleration)
+	else:
+		velocity = velocity.lerp(Vector2.ZERO, friction)
 	move_and_slide()
