dfb4d81dd4f38b754f1c5ba150db045e9260d881
Tomas Bubenicek <tom@tombuben.eu>
2025-01-24 23:09:15 +0100

    npc bubble slot

--- npc_character.gd
+++ npc_character.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
+bubble_slot : Node2D = $BubbleSlot
+
 signal npc_triggered
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	emit_signal("npc_triggered")
 	print("npc triggered")
+	
+func show_bubble():
+	bubble_slot.hide
