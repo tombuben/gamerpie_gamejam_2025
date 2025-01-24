9cb0c48eb406cc8693bdb5f84b6884300ee91693
Tomas Bubenicek <tom@tombuben.eu>
2025-01-24 23:51:02 +0100

    Add npc exit event

--- npc_character.gd
+++ npc_character.gd
@@ -2,11 +2,11 @@ extends Node2D
 
 @onready var bubble_slot : Node2D = $BubbleSlot
 
-signal npc_triggered
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print(body.name)
-	emit_signal("npc_triggered")
+	if body.name != "PlayerCharacter":
+		return
 	DialogManager.start_dialog(global_position, $BubbleSlot.lines)
-	print("npc triggered")
-	print(name)
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	print("npc exited")
