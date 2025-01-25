40592c1f1a6e2afdb3b670ed898844bfea0d55c3
Tomas Bubenicek <tom@tombuben.eu>
2025-01-25 20:53:40 +0100

    You can get to lvl 2 with intros

---
+++ scripts/win_area.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+@export var scene: PackedScene
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name != "PlayerCharacter":
+		return
+	
+	get_tree().change_scene_to_packed(scene)
