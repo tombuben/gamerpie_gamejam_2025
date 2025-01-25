8262c8b5b0284a4de7ed33557f051a7e5cafeadc
LordWizardy <l.wizardy@gmail.com>
2025-01-25 14:19:45 +0100

    Added refactoring for dialog manager, split to separate for bubble slot

--- scripts/player_character.gd
+++ scripts/player_character.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
-@onready var bubble_slot : Node2D = $PlayerBubbleSlot/Bubble
+@onready var bubble_slot : Node2D = $BubbleSlot
 
 @export var player_speed = 200
 @export var friction = 0.01
 @export var acceleration = 0.1
 
+@export var npc_parent = null
 @export var npc_bubble_slot = null
 
 func get_input():
@@ -28,7 +29,8 @@ func _physics_process(_delta):
 		velocity = velocity.lerp(Vector2.ZERO, friction)
 	move_and_slide()
 	
-func _get_active_npc_bubble(bubble_slot: Node2D):
+func _get_active_npc_bubble(parent: Node2D, bubble_slot: Node2D):
+	npc_parent = parent
 	npc_bubble_slot = bubble_slot
 	if npc_bubble_slot != null:
 		print("npc bubble slot saved")
@@ -42,18 +44,17 @@ func _process(delta):
 		_bubble_switch()
 
 func _bubble_switch():
+	var my_bubble_slot = bubble_slot
+	var other_bubble_slot = npc_bubble_slot
 	var npc_bubble = npc_bubble_slot.bubble
-	if npc_bubble != null:
-		print(npc_bubble.lines[0])
-		var tempLine = bubble_slot.lines[0]
-		var tempCheck = bubble_slot.checkValue
-		bubble_slot.lines[0] = npc_bubble.lines[0]
-		bubble_slot.checkValue = npc_bubble.checkValue
-		npc_bubble.lines[0] = tempLine
-		npc_bubble.checkValue = tempCheck
-		print(bubble_slot.lines[0])
-		$PlayerBubbleSlot.load_bubble(bubble_slot.global_position)
-		npc_bubble_slot._bubble_slot_changed(npc_bubble.checkValue)
-	else:
-		print("npc bubble empty")
+	var player_bubble = bubble_slot.bubble
 	
+	my_bubble_slot.remove_child(player_bubble)
+	other_bubble_slot.remove_child(npc_bubble)
+	
+	my_bubble_slot.add_child(npc_bubble)
+	other_bubble_slot.add_child(player_bubble)
+	other_bubble_slot.bubble = player_bubble
+		
+	my_bubble_slot.start_dialog()
+	npc_bubble_slot._bubble_slot_changed(npc_bubble_slot.bubble.checkValue)
--- ui/text box/text_box.gd
+++ ui/text box/text_box.gd
@@ -3,7 +3,7 @@ extends MarginContainer
 @onready var label = $MarginContainer/Label
 @onready var timer = $LetterDisplayTimer
 
-const MAX_WIDTH = 256
+const MAX_WIDTH = 400
 
 var text = ""
 var letter_index = 0
