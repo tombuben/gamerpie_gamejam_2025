eb5d25b4c40e160aa818afbd3ccf581f270ddcfe
LordWizardy <l.wizardy@gmail.com>
2025-01-25 22:07:33 +0100

    Added dedicated swap ui interface

---
+++ ui/swap box/swap_box.gd
@@ -0,0 +1,22 @@
+extends MarginContainer
+
+@export var max_width = 400
+
+var text = ""
+@export var label : Label
+
+func display_text(text_to_display : String):
+	text = text_to_display
+	label.text = text_to_display
+	
+	await resized
+	custom_minimum_size.x = min(size.x, max_width)
+	
+	if size.x > max_width:
+		label.autowrap_mode = TextServer.AUTOWRAP_WORD
+		await resized # wait for x resize
+		await resized # wait for y resize
+		custom_minimum_size.y = size.y
+	
+	#global_position.x -= size.x / 2
+	#global_position.y -= size.y + 24
