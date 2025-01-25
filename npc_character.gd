747f2b83b1971de553b743e73b503920ba44b8c5
LordWizardy <l.wizardy@gmail.com>
2025-01-25 09:26:41 +0100

    Added player bubble and bubble switch function with npc

--- npc_character.gd
+++ npc_character.gd
@@ -6,6 +6,7 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name != "PlayerCharacter":
 		return
 	DialogManager.start_dialog(global_position, $BubbleSlot.lines)
+	body._get_active_npc_bubble(bubble_slot)
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
--- scripts/player_character.gd
+++ scripts/player_character.gd
@@ -1,8 +1,12 @@
 extends CharacterBody2D
 
+@onready var bubble_slot : Node2D = $PlayerBubbleSlot
+
 @export var player_speed = 200
 @export var friction = 0.01
 @export var acceleration = 0.1
+
+@export var npc_bubble = null
 
 func get_input():
 	var input = Vector2()
@@ -23,3 +27,28 @@ func _physics_process(_delta):
 	else:
 		velocity = velocity.lerp(Vector2.ZERO, friction)
 	move_and_slide()
+	
+func _get_active_npc_bubble(bubble: Node2D):
+	npc_bubble = bubble
+	print("npc bubble got")
+	print(npc_bubble.name)
+	print(npc_bubble.checkValue)
+	if npc_bubble != null:
+		print("npc bubble saved")
+	else:
+		print("npc bubble not saved")
+	
+	
+func _process(delta):
+	if Input.is_action_just_pressed('bubble_switch'):
+		print("bubble_switch")
+		_bubble_switch()
+
+func _bubble_switch():
+	if npc_bubble != null:
+		print(npc_bubble.lines[0])
+		$PlayerBubbleSlot.bubble_line = npc_bubble.lines[0]
+		print($PlayerBubbleSlot.bubble_line)
+	else:
+		print("npc bubble empty")
+	
