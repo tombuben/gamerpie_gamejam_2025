99a94aafca58106f64802638d3c76edf30af3e3a
Tomas Bubenicek <tom@tombuben.eu>
2025-01-26 16:00:32 +0100

    Added sounds for lvl 3

--- scripts/singletons/AudioDialogManager.gd
+++ scripts/singletons/AudioDialogManager.gd
@@ -8,7 +8,17 @@ var base_path = "res://assets/audio/dialogue/"
 var audio_dict = {
 	"Come here, you rat!":"_ComeHereYouRat_FINAL.wav",
 	"You are never getting out of here!":"_YouAreNever_FINAL.wav",
-	"Squeek!":"_Squeek_FINAL.wav"
+	"Squeek!":"_Squeek_FINAL.wav",
+	
+	"Here's your money.":"_HeresYourMoney_FINAL.mp3",
+	"Vault number: 1234":"_VaultNumber1234_FINAL.mp3",
+	"Vault number: 4321":"_VaultNumber4321_FINAL.mp3",
+	"Vault number: 9999":"_VaultNumber9999_FINAL.mp3",
+	"Norman Person":"_NormanPerson_FINAL.mp3",
+	"News!":"_News_FINAL.mp3",
+	"Big treasure stolen!":"_BigTreasureStolen_FINAL.mp3",
+	"Suspect:":"_Suspect_FINAL.mp3",
+	"Pirate Piratesson":"_PiratePiratesson_FINAL.mp3",
 }
 
 func play_dialogue_audio(line : String, npc_name : String):
